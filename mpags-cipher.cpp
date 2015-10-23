bbd04f67a996466df979f89a6e671453e2b6720a
Maria Brigida <mbb@hep.ph.bham.ac.uk>
2015-10-23 13:32:37 +0100

    add mpags-cipher.cpp which now is a simple helloworld

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,8 +1,14 @@
 //! Main implementation of the mpags-cipher program
 
 // This is one of the two valid signatures for main()
-int main(int /*argc*/, char* /*argv*/[]) {
+//int main(int /*argc*/, char* /*argv*/[]) {
   // No requirement to return from main, but we do so for clarity and
   // consistency with other functions.
-  return 0;
+// return 0;
+//}
+
+#include <iostream>
+int main()
+{
+  std::cout << "Hello World!\n";
 }
