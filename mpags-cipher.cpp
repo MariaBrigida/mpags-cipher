efadce3135fe6081c17b004d06aeea3ea48c19d5
Maria Brigida <mbb@hep.ph.bham.ac.uk>
2015-10-23 14:27:27 +0100

    modify mpags-cipher.cpp

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -8,7 +8,26 @@
 //}
 
 #include <iostream>
+#include <string>
 int main()
 {
   std::cout << "Hello World!\n";
+  int a{3};
+  std::cout << "a = " << a << "\n";
+  a = 5;
+  std:: cout << "a = " << a << "\n";
+  const double b{2.17};
+  std::cout << "b = " << b << "\n";
+  std::cout << "a*b = " << a*b << "\n";
+  int c{a*b};
+  std::cout << "int c = a*b = " << c << "\n";
+  auto d{true};
+  std::cout << "a/b = " << a/b << "\n";
+  int e{4};
+  std::cout << "e = " << e << "\n";
+  std::cout << "a/e = " << a/e << "\n";
+  std::string estr{"milkshake"};
+  char s{estr[4]};
+  std::cout << "s = " << s << "\n";
+  
 }
