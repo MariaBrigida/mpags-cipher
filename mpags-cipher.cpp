b556f462b1873444246d44ff1955de7adfe7d6f8
Maria Brigida <mbb@hep.ph.bham.ac.uk>
2015-10-23 15:48:28 +0100

    make mpags-cipher convert strings for cipher

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -7,27 +7,66 @@
 // return 0;
 //}
 
+//This program converts makes an input string ready to be encrypted
+
 #include <iostream>
 #include <string>
 int main()
 {
-  std::cout << "Hello World!\n";
-  int a{3};
-  std::cout << "a = " << a << "\n";
-  a = 5;
-  std:: cout << "a = " << a << "\n";
-  const double b{2.17};
-  std::cout << "b = " << b << "\n";
-  std::cout << "a*b = " << a*b << "\n";
-  int c{a*b};
-  std::cout << "int c = a*b = " << c << "\n";
-  auto d{true};
-  std::cout << "a/b = " << a/b << "\n";
-  int e{4};
-  std::cout << "e = " << e << "\n";
-  std::cout << "a/e = " << a/e << "\n";
-  std::string estr{"milkshake"};
-  char s{estr[4]};
-  std::cout << "s = " << s << "\n";
+  std::string out{""};
+  char in_char{'x'};
+  std::cout << "insert a string" << "\n";
   
+  //loop on input string characters
+  while (std::cin >> in_char){
+
+    std::cout << "in_char = " << in_char << "\n";
+    
+    //check if it is a letter and convert to uppercase if it is lowercase
+    //then add to output string
+    if(isalpha(in_char)){
+      in_char = toupper(in_char);
+      std::cout << "new in_char = " << in_char << "\n";
+      out+=in_char;
+    }
+    //check if it is a number and convert it into a word (string)
+    //then add to output string
+    else if(isdigit(in_char)){
+      switch(in_char){
+        case '0':
+	  out+="ZERO";
+	  break;
+        case '1':
+	  out+="ONE";
+	  break;
+        case '2':
+	  out+="TWO";
+	  break;
+        case '3':
+	  out+="THREE";
+	  break;
+        case '4':
+	  out+="FOUR";
+	  break;
+        case '5':
+	  out=+"FIVE";
+	  break;
+        case '6':
+	  out=+"SIX";
+	  break;
+        case '7':
+	  out=+"SEVEN";
+	  break;
+        case '8':
+	  out+="EIGHT";
+	  break;
+        case '9':
+	  out=+"NINE";
+	  break;
+      }
+    }
+    std::cout << " " << "\n";
+  }
+  std::cout << "output string = " << out << "\n";
+  return 0;
 }
